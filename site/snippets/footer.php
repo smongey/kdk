696e8a1e66b62c2e86dd1c0389c07c7e531fcf46
smongey <sean@aokstudio.com>
2016-04-08 10:42:57 +0100

    updates

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -25,14 +25,13 @@
       <div class="col"></div>
     </div>
 
-    <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
     <script>
       (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
       function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
       e=o.createElement(i);r=o.getElementsByTagName(i)[0];
       e.src='//www.google-analytics.com/analytics.js';
       r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
-      ga('create','UA-XXXXX-X');ga('send','pageview');
+      ga('create','UA-76161034-1');ga('send','pageview');
     </script>
 
     <script src="<?php echo url() ?>/dist/scripts/vendor.js"></script>
